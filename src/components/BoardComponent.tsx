3a4be4a61daabb6d062b1b8fe0e0f9be17e33b15
Anton <kramarenko4362@gmail.com>
2022-06-08 14:57:24 +0300

    first commit

---
+++ src/components/BoardComponent.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import { Board } from '../models/Board'
+import { Cell } from '../models/Cell';
+import CellComponent from './CellComponent';
+
+interface BoardProps {
+    board: Board;
+    setBoard: (board: Board) => void
+}
+
+const BoardComponent: React.FC<BoardProps> = ({board, setBoard}) =>{
+    const [selectedCell, setSelectedCell] = useState<Cell | null>(null)
+
+    function click(cell: Cell) {
+        if(cell.figure){
+            setSelectedCell(cell)
+        }
+        
+    }
+
+
+    return (
+        <div className='board'>
+           {board.cells.map((row, index)=>
+           <React.Fragment key={index}>
+               {row.map(cell => 
+                <CellComponent 
+                    click={click}
+                    cell={cell}
+                    key={cell.id}
+                    selected={cell.x === selectedCell?.x && cell.y === selectedCell?.y}
+                />
+                )}
+           </React.Fragment>
+           )}
+        </div>
+    )
+}
+
+export default BoardComponent
